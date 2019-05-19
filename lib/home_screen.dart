9f564aa85089255c9fc822e18124c5453e4b9ba4
kolmakovruslan <ya.ruskol@yandex.ru>
2019-05-19 13:00:06 +0500

    finish fucking mvp

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,56 +8,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _selectedValue = "2";
+  Object _selectedValue = "Ekaterinburg";
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: Text("DRHMT"),
+          title: Text("District17"),
         ),
         body: Stack(
           fit: StackFit.expand,
           children: <Widget>[
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(bottom: 80),
-                child: Text(
-                  "Выбирете город",
-                  textScaleFactor: 1.6,
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                child: DropdownButton(
-                  items: [
-                    DropdownMenuItem(
-                      value: "http://51.68.172.115/hack/Ekaterinburg.districts.json",
-                      child: Text("Екатеринбург"),
-                    ),
-                    DropdownMenuItem(
-                      value: "2",
-                      child: Text("Карабаш"),
-                    ),
-                    DropdownMenuItem(
-                      value: "3",
-                      child: Text("Челябинск"),
-                    ),
-                    DropdownMenuItem(
-                      value: "4",
-                      child: Text("Череповец"),
-                    ),
-                    DropdownMenuItem(
-                      value: "5",
-                      child: Text("Арзамас"),
+            Container(
+              child: Center(
+                child: Row(
+                  children: <Widget>[
+                    Text("Твой город  ", textScaleFactor: 1.2),
+                    DropdownButton(
+                      items: [
+                        DropdownMenuItem(
+                          value: "Ekaterinburg",
+                          child: Text("Екатеринбург"),
+                        ),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedValue = value;
+                        });
+                      },
+                      value: _selectedValue,
                     )
                   ],
-                  onChanged: (value) {
-                    setState(() {
-                      _selectedValue = value;
-                    });
-                  },
-                  value: _selectedValue,
+                  mainAxisAlignment: MainAxisAlignment.center,
                 ),
               ),
             ),
@@ -71,7 +52,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 textColor: Colors.white,
                 onPressed: () {
                   Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => MapScreen(_selectedValue)),
+                    MaterialPageRoute(
+                        builder: (context) => MapScreen(_selectedValue)),
                   );
                 },
               ),
--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -6,26 +6,33 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
 class MapScreen extends StatefulWidget {
-  final String infoLink;
+  final String _where;
 
-  MapScreen(this.infoLink);
+  MapScreen(this._where);
 
   @override
   State<StatefulWidget> createState() {
-    return _MapScreenState(infoLink);
+    return _MapScreenState(_where);
   }
 }
 
 class _MapScreenState extends State<MapScreen> {
-  final String infoLink;
+  final String _where;
   PolylineId _polyline;
+  String _kind = "districts";
+  int _nav = 0;
+  Map<String, dynamic> _data;
 
-  _MapScreenState(this.infoLink);
+  _MapScreenState(this._where);
 
-  Future<Set<Polyline>> loadDistricts() async {
-    final String data = (await http.get(infoLink)).body;
-    final districts = jsonDecode(data)["districts"];
-    final polyline = districts.map((data) {
+  Future<Set<Polyline>> loadLayer() async {
+    final url = "http://51.68.172.115/hack/$_where.$_kind.json";
+    final String data = utf8.decode((await http.get(
+      url,
+    ))
+        .bodyBytes);
+    final items = jsonDecode(data)[_kind];
+    final polyline = items.map((data) {
       final List<LatLng> points = data["coords"].map<LatLng>((dot) {
         final xy = dot.toString().split(", ");
         final x = double.parse(xy[1]);
@@ -33,19 +40,21 @@ class _MapScreenState extends State<MapScreen> {
         return LatLng(x, y);
       }).toList();
       final id = PolylineId(data["id"] + data["name"]);
-
       return Polyline(
         polylineId: id,
         points: points,
-        color: id.value == _polyline?.value ? hexToColor(data["color"]) : Colors.black,
+        color: id.value == _polyline?.value
+            ? hexToColor(data["color"]).withAlpha(50)
+            : Colors.white12,
         width: 8,
         geodesic: true,
         consumeTapEvents: true,
         onTap: () {
           setState(() {
             _polyline = id;
+            _data = data;
           });
-        }
+        },
       );
     });
     final Set<Polyline> set = Set.from(polyline);
@@ -56,35 +65,140 @@ class _MapScreenState extends State<MapScreen> {
     return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
   }
 
+  String getTitle() {
+    final name = _data["name"];
+    if (name == "None") {
+      return "Сквер";
+    }
+    return _data["name"];
+  }
+
+  String getArea() {
+    return "Площадь: ${_data["area"]} km2";
+  }
+
+  String getDescription() {
+    final services = _data["services"];
+    if (services != null) {
+      return "Кол-во сервисов $services";
+    }
+    final good = double.parse(_data["goodstreets"]);
+    final amount = double.parse(_data["streets"]);
+    final percents = (good / amount * 100);
+    return "👍 ${percents.toStringAsFixed(0)}% из ${_data["streets"]} улиц";
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: FutureBuilder(
-          future: loadDistricts(),
-          builder: (context, snapshot) {
-            if (snapshot.error != null) {
-              return Center(
-                child: Text(
-                  snapshot.error.toString(),
-                ),
-              );
-            }
-            if (!snapshot.hasData) {
-              return Center(child: Text("Loading . . ."));
-            }
-            return GoogleMap(
-              mapType: MapType.hybrid,
-              polylines: snapshot.data,
-              myLocationButtonEnabled: false,
-              zoomGesturesEnabled: true,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(56.8390998, 60.6068993),
-                zoom: 15,
-              ),
-              onTap: (latlng) {
-
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            FutureBuilder(
+              future: loadLayer(),
+              builder: (context, snapshot) {
+                if (snapshot.error != null) {
+                  return Center(
+                    child: Text(
+                      snapshot.error.toString(),
+                    ),
+                  );
+                }
+                if (!snapshot.hasData) {
+                  return Center(child: Text("Loading . . ."));
+                }
+                return GoogleMap(
+                  mapType: MapType.hybrid,
+                  polylines: snapshot.data,
+                  myLocationButtonEnabled: false,
+                  zoomGesturesEnabled: true,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(56.8390998, 60.6068993),
+                    zoom: 12,
+                  ),
+                );
               },
-            );
+            ),
+            _data == null
+                ? Container()
+                : PositionedDirectional(
+                    bottom: 0,
+                    start: 0,
+                    end: 0,
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        left: 12,
+                        right: 12,
+                        top: 16,
+                        bottom: 50,
+                      ),
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            alignment: Alignment.topRight,
+                            child: GestureDetector(
+                              child: IconButton(icon: Icon(Icons.close)),
+                              onTap: () {
+                                setState(() {
+                                  _data = null;
+                                });
+                              },
+                            ),
+                          ),
+                          Container(
+                            child: Text(
+                              getTitle(),
+                              textScaleFactor: 2,
+                            ),
+                            alignment: Alignment.topLeft,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(top: 16),
+                            child: Text(getArea()),
+                            alignment: Alignment.topLeft,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(top: 16),
+                            child: Text(
+                              getDescription(),
+                              textScaleFactor: 1.5,
+                            ),
+                            alignment: Alignment.topLeft,
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.domain),
+              title: Text("Районы"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.filter_hdr),
+              title: Text("Парки"),
+            ),
+          ],
+          onTap: (index) {
+            if (index == 0) {
+              setState(() {
+                _kind = "districts";
+                _nav = 0;
+                _data = null;
+              });
+            } else {
+              setState(() {
+                _kind = "gardens";
+                _nav = 1;
+                _data = null;
+              });
+            }
           },
+          currentIndex: _nav,
         ),
       );
 }
